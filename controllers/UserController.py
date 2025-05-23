5e6fc6dda22a37c1312abd4d70ad5ea3cdba5e44
Albert <albertjokro22@gmail.com>
2025-05-23 19:28:09 +0700

    perubahan file app.py

--- controllers/UserController.py
+++ controllers/UserController.py
@@ -1,34 +1,31 @@
-from controllers.Controller import Controller
-from db.db import SessionLocal
-from flask import request, jsonify
+from controllers.Controllers import Controller
+from flask import jsonify
 from models.UsersModel import User
 
-class CreateUserController(Controller):
+class UserController(Controller):
     def __init__(self):
-        self.__db = SessionLocal()
-        self.data = request.json
+        super().__init__()
 
-    def CreateUser(self):
+    def Create(self):
         new_user = User(
             nama=self.data['nama'],
             email=self.data['email'],
             username=self.data['username'],
             password=hash(self.data['password']),
         )
-        self.__db.add(new_user)
-        self.__db.commit()
-        self.__db.refresh(new_user)
-        self.__db.close()
+        self._db.add(new_user)
+        self._db.commit()
+        self._db.refresh(new_user)
+        self._db.close()
 
         return jsonify({
             "msg": "User Created",
             "id": new_user.id,
             "code": 201
-        }, 201)
+        }), 201
 
-    def GetUser(self):
-
+    def Get(self):
         return jsonify({
             "msg": "User Found",
             "code": 200
-        })
+        }), 200
