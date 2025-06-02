f81d6110e162479f200d9cea8c3aec44785f7c44
Xenoniums <s32240008@student.ubm.ac.id>
2025-06-02 23:20:13 +0700

    frontend
    yang terselesaikan dashboard, login, regis (frontend)

--- app.py
+++ app.py
@@ -1,27 +1,56 @@
-from flask import Flask
-from controllers.UserController import UserController
-from db.db import engine
-from models.UsersModel import Base
+from flask import Flask, flash, redirect, render_template, request, url_for, session
+from config import Config
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import MySQLdb.cursors, re, hashlib
 
-class EventWebsite:
+class Portal:
     def __init__(self):
         self.app = Flask(__name__)
-        self.app.secret_key = 'super_secret_key'
+        self.app.secret_key = '1234'
+        self.app.config.from_object(Config)
+        self.mysql = MySQL(self.app)
         self.routes()
-        Base.metadata.create_all(bind=engine)
-
-
+        
     def routes(self):
-        app = self.app
-        @app.route("/api/user/create", methods=["POST"])
-        def register_user():
-            user = UserController()
-            return user.Create()
-
+        @self.app.route('/testdb')
+        def test_db():
+            try:
+                if self.con is not None:
+                    return "Database connection is successful!"
+            except Exception as e:
+                return f"Database connection failed: {e}"
+                
+        @self.app.route('/login',  methods=['GET', 'POST'])
+        def login():
+            msg = ''
+            if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+                username = request.form['username']
+                password = request.form['password']
+                cursor = self.mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+                cursor.execute('SELECT * FROM user WHERE username = %s AND password = %s', (username, password,))
+                account = cursor.fetchone()
+                if account:
+                    session['loggedin'] = True
+                    session['id'] = account['id']
+                    session['username'] = account['username']
+                    return redirect(url_for('dashboard'))
+                else:
+                    msg = 'Incorrect username/password!'
+            return render_template('login.html', msg=msg)
+        
+        @self.app.route('/login/createaccount', methods=['GET', 'POST'])
+        def createaccount():
+            msg = 'aduh error'
+            return render_template('createaccount.html')
+        
+        @self.app.route('/dashboard')
+        def dashboard():
+            return render_template('dashboard.html')
+    
     def run(self):
-        self.app.run(debug=True, port=5555)
-
-
+        self.app.run(debug=True)
+        
 if __name__ == '__main__':
-    eventWeb = EventWebsite()
-    eventWeb.run()
+    portal = Portal()
+    portal.run()
