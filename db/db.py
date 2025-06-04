0944504ead84091dedcad6d0406b85b6b2271bd7
Albert <albertjokro22@gmail.com>
2025-06-04 08:15:21 +0700

    creating file env_local.py dan mengupdate file db.py

--- db/db.py
+++ db/db.py
@@ -1,12 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DB_USER = 'root'
-DB_PASS = 'root123'
-DB_HOST = 'localhost'
-DB_NAME = 'tiket_event'
+from env_local import DB_USER, DB_PASS, DB_HOST, DB_NAME
 
 # Format MySQL URL
+from sqlalchemy import create_engine, text
+
+# URL awal tanpa DB_NAME untuk membuat database
+DATABASE_URL = f'mysql+mysqlconnector://{DB_USER}:{DB_PASS}@{DB_HOST}'
+engine = create_engine(DATABASE_URL)
+
+# Buat database jika belum ada
+with engine.connect() as connection:
+    connection.execute(text(f"CREATE DATABASE IF NOT EXISTS {DB_NAME}"))
+
+# Setelah database dibuat, buat engine baru yang terhubung ke database tersebut
 DATABASE_URL = f'mysql+mysqlconnector://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}'
 engine = create_engine(DATABASE_URL)
 
---
+++ env_local.py
@@ -0,0 +1,4 @@
+DB_USER = 'root'
+DB_PASS = 'root123' # ganti dengan '' jika database tidak memiliki password
+DB_HOST = 'localhost'
+DB_NAME = 'tiket_event'
